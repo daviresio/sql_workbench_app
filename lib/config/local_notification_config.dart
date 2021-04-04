5ee81977f7f6192f456b08040e48835d7f488203
Davi Resio <daviresio@gmail.com>
2021-04-04 02:27:03 -0300

    add ios support

--- lib/config/local_notification_config.dart
+++ lib/config/local_notification_config.dart
@@ -4,11 +4,12 @@ import 'package:open_file/open_file.dart';
 
 const ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_ID = '1';
 const ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_NAME = 'Download Concluido';
-const ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_DESCRIPTION = 'Notification showed when download file was completed';
+const ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_DESCRIPTION =
+    'Notification showed when download file was completed';
 
 class LocalNotificationConfig {
-
-  static final LocalNotificationConfig _localNotificationConfig = LocalNotificationConfig._internal();
+  static final LocalNotificationConfig _localNotificationConfig =
+      LocalNotificationConfig._internal();
 
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
@@ -19,32 +20,46 @@ class LocalNotificationConfig {
   LocalNotificationConfig._internal() {
     flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-    var initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/launcher_icon');
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/launcher_icon');
+
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: true,
+      requestBadgePermission: true,
+      requestAlertPermission: true,
+    );
 
     var initializationSettings = InitializationSettings(
       android: initializationSettingsAndroid,
-      iOS: null,
+      iOS: initializationSettingsIOS,
     );
-    flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: selectNotification);
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
   }
 
   Future selectNotification(String payload) async {
-    if(payload == null) return;
+    if (payload == null) return;
 
-      if(payload.endsWith('.csv')) {
-        OpenFile.open('${await PathProviderConfig.localPath}/$payload', type: 'application/vnd.ms-excel');
-      }
+    if (payload.endsWith('.csv')) {
+      OpenFile.open('${await PathProviderConfig.localPath}/$payload',
+          type: 'application/vnd.ms-excel');
+    }
 
-      if(payload.endsWith('.json')) {
-        OpenFile.open('${await PathProviderConfig.localPath}/$payload', type: 'text/plain');
-      }
-
+    if (payload.endsWith('.json')) {
+      OpenFile.open('${await PathProviderConfig.localPath}/$payload',
+          type: 'text/plain');
+    }
   }
 
   showNotification(String filename) async {
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_ID, ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_NAME, ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_DESCRIPTION,
-      importance: Importance.high, priority: Priority.high,
+      ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_ID,
+      ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_NAME,
+      ANDROID_NOTIFICATION_DOWNLOAD_CHANEL_DESCRIPTION,
+      importance: Importance.high,
+      priority: Priority.high,
     );
 
     var platformChannelSpecifics = NotificationDetails(
@@ -52,9 +67,8 @@ class LocalNotificationConfig {
       iOS: null,
     );
 
-    await flutterLocalNotificationsPlugin.show(0, 'Download Concluido!', 'Download file $filename was concluded!',
-        platformChannelSpecifics, payload: filename);
-
+    await flutterLocalNotificationsPlugin.show(0, 'Download Concluido!',
+        'Download file $filename was concluded!', platformChannelSpecifics,
+        payload: filename);
   }
-
 }
--- lib/pages/connection_home/pages/query/query_page.dart
+++ lib/pages/connection_home/pages/query/query_page.dart
@@ -16,12 +16,13 @@ import 'widgets/download_result_query_result/dialog_download_result.dart';
 import 'widgets/view_row/dialog_view_row.dart';
 
 class QueryPage extends StatefulWidget {
-
   final int databaseInfoId;
   final int connectionId;
   final Function setQuerySaved;
 
-  QueryPage({Key key, this.databaseInfoId, this.connectionId, this.setQuerySaved}) : super(key: key);
+  QueryPage(
+      {Key key, this.databaseInfoId, this.connectionId, this.setQuerySaved})
+      : super(key: key);
 
   final TextEditingController queryEditingController = TextEditingController();
 
@@ -30,7 +31,6 @@ class QueryPage extends StatefulWidget {
 }
 
 class _QueryPageState extends State<QueryPage> {
-
   final GlobalKey<State> _globalKey = GlobalKey<State>();
   final QuerySavedDao _querySavedDao = Database.instance.querySavedDao;
 
@@ -46,43 +46,44 @@ class _QueryPageState extends State<QueryPage> {
       });
     });
     super.initState();
-
   }
-
 
   @override
   Widget build(BuildContext context) {
-
     _controller = Provider.of<QueryStore>(context);
 
     _scrollToTop() {
-      _scrollController.animateTo(0.0, duration: Duration(milliseconds: 500), curve: Curves.easeOut);
+      _scrollController.animateTo(0.0,
+          duration: Duration(milliseconds: 500), curve: Curves.easeOut);
     }
 
     _scrollToBottom() {
-      _scrollController.animateTo(_scrollController.position.maxScrollExtent, duration: Duration(milliseconds: 500), curve: Curves.easeOut);
+      _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+          duration: Duration(milliseconds: 500), curve: Curves.easeOut);
     }
 
     _saveQuery(QuerySaved querySaved) async {
       var value = widget.queryEditingController.text.trim();
 
-      if(value == '') {
+      if (value == '') {
         ShowFlushbar.error(message: 'Query is empty!').show(context);
         return;
       }
 
-      if(querySaved.id != null && querySaved.id > 0) {
+      if (querySaved.id != null && querySaved.id > 0) {
         var result = await _querySavedDao.edit(querySaved);
-        _controller.setQuery(_controller.querySaved.copyWith(id: result, query: value));
+        _controller.setQuery(
+            _controller.querySaved.copyWith(id: result, query: value));
       } else {
-        var result = await _querySavedDao.add(querySaved.copyWith(query: value));
+        var result =
+            await _querySavedDao.add(querySaved.copyWith(query: value));
       }
     }
 
     _handleQuery([value]) async {
       value ??= widget.queryEditingController.text.trim();
 
-      if(value == '') {
+      if (value == '') {
         ShowFlushbar.error(message: 'Query is empty!').show(context);
         return;
       }
@@ -90,7 +91,8 @@ class _QueryPageState extends State<QueryPage> {
       try {
         Dialogs.showLoadingDialog(context, _globalKey);
         QueryModel query = QueryModel();
-        var connection =  await Database.instance.connectionDao.find(widget.connectionId);
+        var connection =
+            await Database.instance.connectionDao.find(widget.connectionId);
         query.connection = ConnectionModel.fromTable(connection);
         query.query = value;
 
@@ -101,22 +103,25 @@ class _QueryPageState extends State<QueryPage> {
 
         _controller.setLastQueryExecuted(value);
 
-        if(_controller.rows.length > 0) {
+        if (_controller.rows.length > 0) {
           //TODO ANALISAR E DEPOIS DECIDIR SE `E MELHOR SALVAR OU NAO NO HISTORICO QUERYS QUE NAO TENHAM LINHAS RETORNADAS
 //          _saveQuery(QuerySaved(query: value, databaseInfoId: widget.databaseInfoId));
           await Future.delayed(Duration(milliseconds: 500));
           _scrollToBottom();
         }
-
-      } catch(e) {
+      } catch (e, s) {
         Dialogs.errorDialog(e, context);
       }
     }
 
     showDialogViewRow(Map<String, dynamic> row) async {
-      var result = await showViewDialog(context: context, item: row, types: _controller.types, connectionId: widget.connectionId);
-      if(result == true) {
-      _handleQuery(_controller.lastQueryExecuted);
+      var result = await showViewDialog(
+          context: context,
+          item: row,
+          types: _controller.types,
+          connectionId: widget.connectionId);
+      if (result == true) {
+        _handleQuery(_controller.lastQueryExecuted);
       }
     }
 
@@ -124,31 +129,38 @@ class _QueryPageState extends State<QueryPage> {
       floatingActionButton: Observer(
         //TODO exibir com base se a pesquisa foi feita e nao se tem linhas
         builder: (_) {
-          return _controller.lastQueryExecuted != '' ? SpeedDial(
-            marginBottom: 60.0,
-            animatedIcon: AnimatedIcons.menu_close,
-            backgroundColor: Colors.blueAccent,
-            children: [
-              (_controller.isQueryInSingleTable ? SpeedDialChild(
-                elevation: 8.0,
-                backgroundColor: Colors.green,
-                child: Icon(Icons.add),
-                onTap: () {
-                  showViewDialog(context: context, item: null, types: _controller.types, connectionId: widget.connectionId);
-                },
-              ) : Container()),
-              SpeedDialChild(
-                elevation: 8.0,
-                backgroundColor: Colors.cyan,
-                child: Icon(Icons.file_download),
-                onTap: () async {
-                  await showDialogDownloadResultQuery(
-                      context, _controller.rows);
-                },
-              ),
-            ],
-          ) : Container();
-
+          return _controller.lastQueryExecuted != ''
+              ? SpeedDial(
+                  marginBottom: 60.0,
+                  animatedIcon: AnimatedIcons.menu_close,
+                  backgroundColor: Colors.blueAccent,
+                  children: [
+                    (_controller.isQueryInSingleTable
+                        ? SpeedDialChild(
+                            elevation: 8.0,
+                            backgroundColor: Colors.green,
+                            child: Icon(Icons.add),
+                            onTap: () {
+                              showViewDialog(
+                                  context: context,
+                                  item: null,
+                                  types: _controller.types,
+                                  connectionId: widget.connectionId);
+                            },
+                          )
+                        : Container()),
+                    SpeedDialChild(
+                      elevation: 8.0,
+                      backgroundColor: Colors.cyan,
+                      child: Icon(Icons.file_download),
+                      onTap: () async {
+                        await showDialogDownloadResultQuery(
+                            context, _controller.rows);
+                      },
+                    ),
+                  ],
+                )
+              : Container();
         },
       ),
       body: ListView(
@@ -163,7 +175,8 @@ class _QueryPageState extends State<QueryPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 16.0, right: 16.0),
+                    padding: const EdgeInsets.only(
+                        top: 8.0, bottom: 8.0, left: 16.0, right: 16.0),
                     child: TextFormField(
                       controller: widget.queryEditingController,
                       onChanged: _controller.setQuery,
@@ -174,7 +187,8 @@ class _QueryPageState extends State<QueryPage> {
                         suffix: GestureDetector(
                           child: Icon(Icons.close),
                           onTap: () {
-                            WidgetsBinding.instance.addPostFrameCallback((_) => widget.queryEditingController.clear());
+                            WidgetsBinding.instance.addPostFrameCallback(
+                                (_) => widget.queryEditingController.clear());
                           },
                         ),
                       ),
@@ -186,11 +200,12 @@ class _QueryPageState extends State<QueryPage> {
                       FlatButton(
                         child: Text('SAVE'),
                         onPressed: () {
-                          _saveQuery(_controller.querySaved.copyWith(saved: true, databaseInfoId: widget.databaseInfoId));
+                          _saveQuery(_controller.querySaved.copyWith(
+                              saved: true,
+                              databaseInfoId: widget.databaseInfoId));
                         },
                       ),
                       FlatButton(
-
                         child: Text('GO'),
                         onPressed: _handleQuery,
                       ),
@@ -202,11 +217,12 @@ class _QueryPageState extends State<QueryPage> {
           ),
           Observer(
             builder: (_) {
-              if(_controller.columns.length == 0) return Container();
-
+              if (_controller.columns.length == 0) return Container();
 
               return Container(
-                height: MediaQuery.of(context).size.height - Scaffold.of(context).appBarMaxHeight - 20,
+                height: MediaQuery.of(context).size.height -
+                    Scaffold.of(context).appBarMaxHeight -
+                    20,
                 child: Stack(
                   alignment: Alignment.bottomCenter,
                   children: <Widget>[
@@ -216,39 +232,49 @@ class _QueryPageState extends State<QueryPage> {
                         child: SingleChildScrollView(
                             scrollDirection: Axis.horizontal,
                             child: ConstrainedBox(
-                              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
+                              constraints: BoxConstraints(
+                                  maxWidth: MediaQuery.of(context).size.width),
                               child: Observer(
                                 builder: (_) {
-                                  if(_controller.rows.length == 0 && _controller.lastQueryExecuted == '') return Container();
+                                  if (_controller.rows.length == 0 &&
+                                      _controller.lastQueryExecuted == '')
+                                    return Container();
 
-                                  if(_controller.rows.length == 0 && _controller.lastQueryExecuted != '') return Center(
-                                      child:Text(
-                                          'This query return empty result',
-                                        style: TextStyle(
-                                          color: Colors.black45,
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ));
+                                  if (_controller.rows.length == 0 &&
+                                      _controller.lastQueryExecuted != '')
+                                    return Center(
+                                        child: Text(
+                                      'This query return empty result',
+                                      style: TextStyle(
+                                        color: Colors.black45,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ));
 
                                   return PaginatedDataTable(
-                                    sortColumnIndex: _controller.columnSortIndex,
+                                    sortColumnIndex:
+                                        _controller.columnSortIndex,
                                     sortAscending: _controller.columnSortAsc,
-                                    columns: _controller.columns.map((value) =>
-                                        DataColumn(label: Text(value),
+                                    columns: _controller.columns
+                                        .map((value) => DataColumn(
+                                            label: Text(value),
                                             numeric: value.runtimeType == int ||
                                                 value.runtimeType == double,
-                                            onSort: _controller.changeTableSort))
+                                            onSort:
+                                                _controller.changeTableSort))
                                         .toList(),
                                     header: Container(),
-                                    source: QueryResultTable(_controller, showDialogViewRow),
-                                    rowsPerPage: _controller.rows.length < 50 ? _controller.rows.length : 50,
+                                    source: QueryResultTable(
+                                        _controller, showDialogViewRow),
+                                    rowsPerPage: _controller.rows.length < 50
+                                        ? _controller.rows.length
+                                        : 50,
                                     availableRowsPerPage: [25, 50, 75, 100],
                                   );
                                 },
                               ),
-                            )
-                        ),
+                            )),
                       ),
                     ),
                     Positioned(
@@ -266,16 +292,20 @@ class _QueryPageState extends State<QueryPage> {
                           ],
                         ),
                         child: Observer(
-                          builder: (_) => _controller.rows.length > 0 ? IconButton(
-                            icon: _controller.onTop ? Icon(Icons.keyboard_arrow_up) : Icon(Icons.keyboard_arrow_down),
-                            onPressed: () {
-                              if(_controller.onTop) {
-                                _scrollToBottom();
-                              } else {
-                                _scrollToTop();
-                              }
-                            },
-                          ) : Container(),
+                          builder: (_) => _controller.rows.length > 0
+                              ? IconButton(
+                                  icon: _controller.onTop
+                                      ? Icon(Icons.keyboard_arrow_up)
+                                      : Icon(Icons.keyboard_arrow_down),
+                                  onPressed: () {
+                                    if (_controller.onTop) {
+                                      _scrollToBottom();
+                                    } else {
+                                      _scrollToTop();
+                                    }
+                                  },
+                                )
+                              : Container(),
                         ),
                       ),
                     ),
@@ -288,6 +318,4 @@ class _QueryPageState extends State<QueryPage> {
       ),
     );
   }
-
-
 }
